d0cec797a56cf82fb676b7be78125eb45c839219
tuanct413 <145199324+tuanct413@users.noreply.github.com>
2025-08-22 20:20:20 +0700

    Update DemoApplicationTests.java

--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EnableAutoConfiguration(exclude = {
-		DataSourceAutoConfiguration.class,
-		MailSenderAutoConfiguration.class,
-		SecurityAutoConfiguration.class
-})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// chỉ cần lên context
-	}
+    @Test
+    void contextLoads() {
+        // chỉ cần ApplicationContext load thành công
+    }
 }
-
