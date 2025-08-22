06f1cdfe9aaebe5d4f4433988ffdc8e5607aa424
tuanct413 <tuanct413@gmail.com>
2025-08-22 20:01:27 +0700

    thêm biến môi trường

--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@ActiveProfiles("test")  // load application-test.properties
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// chỉ kiểm tra ApplicationContext load thành công
+		// Chỉ test context load, không cần DB thật
 	}
 }
