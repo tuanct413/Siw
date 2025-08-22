404b8cabbb6fb0c583b97b6cf8f613443739e849
tuanct413 <tuanct413@gmail.com>
2025-08-22 19:50:56 +0700

    thêm biến môi trường

--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// chỉ kiểm tra ApplicationContext load thành công
 	}
-
 }
