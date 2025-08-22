355c2f593006578c5f6b56b94a9757302f8a9da5
tuanct413 <tuanct413@gmail.com>
2025-08-22 18:30:07 +0700

    update

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +13,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Application started successfully!");
 	}
+	@Bean
+	public CommandLineRunner checkDataSource(DataSource dataSource) {
+		return args -> {
+			System.out.println("🔎 Database đang kết nối: " + dataSource.getConnection().getMetaData().getURL());
+		};
+	}
+
 
 }
