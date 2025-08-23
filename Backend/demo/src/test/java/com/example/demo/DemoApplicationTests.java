9f264b9202f2a6657dcd4a6e84a33d60bb4d3910
tuanct413 <tuanct413@gmail.com>
2025-08-23 19:13:23 +0700

    update config

--- Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,16 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        // chỉ cần ApplicationContext load thành công
+        // Chỉ load ApplicationContext, không cần database
     }
 }
