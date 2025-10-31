c8ebfb85eec9de89385b9b6a91a9b00ebaf2efdb
tuanct413 <tuanct413@gmail.com>
2025-10-31 16:37:26 +0700

    chuyển url từ localhost sang sever backend

--- frontend/src/api/weather/weatherService.js
+++ frontend/src/api/weather/weatherService.js
@@ -1,7 +1,7 @@
 // src/services/weatherService.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080"; // chỉnh lại nếu khác
+const API_BASE_URL = "https://siw-backend.onrender.com"; // chỉnh lại nếu khác
 
 
 export const fetchWeather = async (cityName) => {
